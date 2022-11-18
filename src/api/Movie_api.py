36c35c3dc2be77bae5693f7ef6f546ae2ec0b143
yoshimi-I <iwatsuki.443@outlook.com>
2022-11-18 12:45:05 +0900

    fix: エラーハンドリングの追加

--- src/api/Movie_api.py
+++ src/api/Movie_api.py
@@ -1,5 +1,6 @@
-from justwatch import JustWatch
 import random
+import requests
+import json
 
 class Recommend:
     def __init__(self,just_watch,content_type,provider,genre,score):
@@ -14,29 +15,40 @@ class Recommend:
 							"min_scoring_value":score,"max_scoring_value":10.0
 							}
 }
-        self.results = just_watch.search_for_item(content_types=[self.content_type],providers=[self.provider],genres=[self.genre],scoring_filter_types=self.score)
+        self.results = self.just_watch.search_for_item(content_types=[self.content_type],providers=[self.provider],genres=[self.genre],scoring_filter_types=self.score)
         self.page_num = self.results["total_pages"]
         self.items = self.results["items"]
 
     def recommend(self):
         L = []
-        for i in range(self.page_num):
-            results2 = self.just_watch.search_for_item(content_types=[self.content_type], providers=[self.provider], genres=[self.genre], scoring_filter_types=self.score, page=i)
-            for j in range(len(results2["items"])):
-                title = results2["items"][j]["title"]
-                L.append(title)
+        i = random.randint(0,self.page_num)
+        print(i,self.page_num)
+        results2 = self.just_watch.search_for_item(content_types=[self.content_type],providers=[self.provider],genres=[self.genre],scoring_filter_types=self.score,page=i)
+        for j in range(len(results2["items"])):
+            title = results2["items"][j]["title"]
+            L.append(title)
         return L
 
     def info(self):
-        watch_info = dict()
+        watch_info = list()
         title_list = self.recommend()
-        num = random.sample(range(len(title_list)), 5)
+        title_num = len(title_list)
+        num = random.sample(range(title_num), title_num)
+        count = 0
         for i in num:
             # recommendメソッドで作った作品名で検索して出てきたurlを保持
             title_name  = title_list[i]
-            title_info = just_watch.search_for_item(query=str(title_name),page=1)
-            #urlを取得
-            url = title_info["items"][0]["offers"][0]["urls"]["deeplink_web"]
+            title_info = self.just_watch.search_for_item(query=str(title_name),page=1)
+            # まずは動画の配信元を探る
+            try:
+                provider = title_info["items"][0]["offers"][0]["package_short_name"]
+            except:
+                continue
+            if provider == self.provider:
+                url = title_info["items"][0]["offers"][0]["urls"]["standard_web"]
+            else:
+                continue
+
             # 評価を取得
             # なぜだかわからないが順番が毎回バラバラ(多分シーズン毎の評価だと思われる)
 
@@ -44,5 +56,89 @@ class Recommend:
             for j in range(len(value_list)):
                 value = value_list[j]["value"]
                 if self.num <= value <= 10:
-                    watch_info[title_name] = {"url":url,"value":value}
+                    watch_info.append({"title":title_name,"url":url,"value":value,"content_type":self.content_type})
+                    break
+            if len(watch_info) == 3:
+                break
         return watch_info
+
+    class TMDB:
+        def __init__(self, token, info):
+            self.token = token
+            self.headers_ = {'Authorization': f'Bearer {self.token}', 'Content-Type': 'application/json;charset=utf-8'}
+            self.base_url_ = 'https://api.themoviedb.org/3/'
+            self.img_base_url_ = 'https://image.tmdb.org/t/p/w500'
+            self.language = 'ja-JA'
+            self.title = info["title"]
+            self.content_type = info["content_type"]
+            self.value = info["value"]
+            self.url = info["url"]
+
+        def _json_by_get_request(self, url, params={}):
+            res = requests.get(url, headers=self.headers_, params=params)
+            return json.loads(res.text)
+
+        def search_movies_posters(self, query):
+            params = {'query': query, "language": self.language}
+            url = f'{self.base_url_}search/movie'
+            res = self._json_by_get_request(url, params)
+            try:
+                poster_path = res["results"][0]["poster_path"]
+                url = self.img_base_url_ + poster_path
+            except:
+                url = "https://generative-placeholders.glitch.me/image?width=600&height=300&style=cellular-automata&cells=10"
+            return url
+
+        def search_movies_description(self, query):
+            params = {'query': query, "language": self.language}
+            url = f'{self.base_url_}search/movie'
+            res = self._json_by_get_request(url, params)
+            try:
+                description = res["results"][0]["overview"]
+            except:
+                description = "概要なし"
+            return description
+
+        def movies_info(self):
+            real_value = round(self.value / 2, 1)
+            img_url = self.search_movies_posters(self.title)
+            movie_outline = self.search_movies_description(self.title)
+            L = {'title': self.title, 'url': self.url, 'value': real_value, 'img_url': img_url,
+                 'movie_outline': movie_outline}
+            return L
+
+        def search_shows_posters(self, query):
+            params = {'query': query, "language": self.language}
+            url = f'{self.base_url_}search/tv'
+            res = self._json_by_get_request(url, params)
+            try:
+                poster_path = res["results"][0]["poster_path"]
+                url = self.img_base_url_ + poster_path
+            except:
+                url = "https://generative-placeholders.glitch.me/image?width=600&height=300&style=cellular-automata&cells=10"
+            return url
+
+        def search_shows_description(self, query):
+            params = {'query': query, "language": self.language}
+            url = f'{self.base_url_}search/tv'
+            res = self._json_by_get_request(url, params)
+            try:
+                description = res["results"][0]["overview"]
+            except:
+                description = "概要なし"
+            return description
+
+        def shows_info(self):
+            real_value = round(self.value / 2, 1)
+            img_url = self.search_shows_posters(self.title)
+            movie_outline = self.search_shows_description(self.title)
+            L = {'title': self.title, 'url': self.url, 'value': real_value, 'img_url': img_url,
+                 'movie_outline': movie_outline}
+            return L
+
+        def info(self):
+            if self.content_type == "movie":
+                return self.movies_info()
+            else:
+                return self.shows_info()
+
--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+from justwatch import JustWatch
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -11,11 +12,16 @@ from linebot.models import (
 )
 from tinydb import TinyDB, Query
 
+from src.api.Movie_api import Recommend, TMDB
+
 app = Flask(__name__)
 
-
+# 環境変数一覧を記載
+LINE_CHANNEL_ACCESS_TOKEN = 'GmwhLIGLw6fm4aEJT2tzqFKp80nw5YY03VGR/BJdWKyAvmqsTTNxS/RnP9U2+bnHVbiig6BskVPgOfQGAM1Gr2iD/vjHulB5PitwNivjc39ihiBQsLkvxJQ/EgOv/tivdiI7UMqkO2DDTqskUHc8UwdB04t89/1O/w1cDnyilFU='
+LINE_CHANNEL_SECRET = '33465f56e279d8139a9b22a3b9e6a9e9'
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1Mzk3ZmQ5ODM3Y2I5NGJjOTQxNGQzNDM1NzQ4MThhYiIsInN1YiI6IjYxY2M3YTFjMzg1MjAyMDBhMjAxNDMyOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fTfSukOtFlhbARDE08wit36Q-UYh6-lGG46NUyZmb00"
 
 # ここからTinyDBの処理
 db = TinyDB("db/user_items.json")
@@ -53,40 +59,150 @@ def handle_message(event: MessageEvent):
         db.insert({"id": user_id, "content_type": None, "genre": None, "providers": None, "review_score": None,"ques_id": 1})
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text='作品の種類を選んでください',
+            TextSendMessage(text='動画の視聴方法を選んでください',
                             quick_reply=QuickReply(items=[
-                                QuickReplyButton(action=MessageAction(label="映画", text="映画")),
-                                QuickReplyButton(action=MessageAction(label="シリーズ", text="シリーズ")),
-                                ])
-                            )
-        )
-    elif db.search(query.id == user_id)[0]["ques_id"] == 1:
-        mes = event.message.text
-        if mes == "映画":
-            content_type = "movie"
-        else:
-            content_type = "show"
-
-        # まずはデータベースをアップデートする,そして受け取った値を保持する必要がある
-        db.update({"content_type":content_type ,"ques_id": 2},query.id == user_id)
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='作品のジャンルを選んでください',
-                            quick_reply=QuickReply(items=[
-                                QuickReplyButton(action=MessageAction(label="アクション", text="アクション")),
-                                QuickReplyButton(action=MessageAction(label="アニメーション", text="アニメーション")),
+                                QuickReplyButton(action=MessageAction(label="アマプラ", text="アマプラ")),
+                                QuickReplyButton(action=MessageAction(label="ネトフリ", text="ネトフリ")),
                             ])
                             )
         )
+
+    elif db.search(query.id == user_id)[0]["ques_id"] == 1:
+        mes = event.message.text
+        # 何かしらの処理で読み込まれなかった時用の保険
+        providers = "null"
+        if mes == "アマプラ":
+            providers = "amp"
+        elif mes == "ネトフリ":
+            providers = "nfx"
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+            TextSendMessage(text='選択肢の中から選んでください',
+                            quick_reply=QuickReply(items=[
+                                QuickReplyButton(action=MessageAction(label="アマプラ", text="アマプラ")),
+                                QuickReplyButton(action=MessageAction(label="ネトフリ", text="ネトフリ")),
+                            ])
+                            )
+            )
+            exit()
+
+        db.update({"providers": providers, "ques_id": 2}, query.id == user_id)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='作品の種類を選んでください',
+                            quick_reply=QuickReply(items=[
+                                QuickReplyButton(action=MessageAction(label="映画", text="映画")),
+                                QuickReplyButton(action=MessageAction(label="その他", text="その他")),
+                            ])
+                            )
+        )
+
     elif db.search(query.id == user_id)[0]["ques_id"] == 2:
+        mes = event.message.text
+        content_type = "null"
+        if mes == "映画":
+            content_type = "movie"
+        elif mes == "その他":
+            content_type = "show"
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='選択肢の中から選んでください',
+                                quick_reply=QuickReply(items=[
+                                    QuickReplyButton(action=MessageAction(label="映画", text="映画")),
+                                    QuickReplyButton(action=MessageAction(label="その他", text="その他")),
+                                ])
+                                )
+            )
+            exit()
+
+
+        # まずはデータベースをアップデートする,そして受け取った値を保持する必要がある
+        db.update({"content_type":content_type ,"ques_id": 3},query.id == user_id)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='作品のジャンルを選んでください(右にスクロールできます)',
+                            quick_reply=QuickReply(items=[
+                                QuickReplyButton(action=MessageAction(label="アクション", text="アクション")),
+                                QuickReplyButton(action=MessageAction(label="アニメーション", text="アニメーション")),
+                                QuickReplyButton(action=MessageAction(label="コメディ", text="コメディ")),
+                                QuickReplyButton(action=MessageAction(label="犯罪,戦争", text="犯罪,戦争")),
+                                QuickReplyButton(action=MessageAction(label="ドキュメンタリー", text="ドキュメンタリー")),
+                                QuickReplyButton(action=MessageAction(label="ドラマ", text="ドラマ")),
+                                QuickReplyButton(action=MessageAction(label="ファンタジー,SF", text="ファンタジー,SF")),
+                                QuickReplyButton(action=MessageAction(label="歴史", text="歴史")),
+                                QuickReplyButton(action=MessageAction(label="ホラー,ミステリー", text="ホラー,ミステリー")),
+                                QuickReplyButton(action=MessageAction(label="ファミリー", text="ファミリー")),
+                                QuickReplyButton(action=MessageAction(label="ミュージカル", text="ミュージカル")),
+                                QuickReplyButton(action=MessageAction(label="ロマンス", text="ロマンス")),
+                                QuickReplyButton(action=MessageAction(label="スポーツ", text="スポーツ")),
+
+                            ])
+                            )
+        )
+    elif db.search(query.id == user_id)[0]["ques_id"] == 3:
 
         # 受け取る値は1つまえの選択肢となる
         mes = event.message.text
+
+        # とりあえず初期化
+        genre = "null"
+
+        # 以下受け取った値をもとにDBへ格納する文字列を決める(本当は直接書きたかったけど,文字がバイト形で入るので無理だった)
         if mes == "アクション":
             genre = "act"
         elif mes == "アニメーション":
             genre = "ani"
-        db.update({"genre": genre, "ques_id": 3}, query.id == user_id)
+        elif mes == "コメディ":
+            genre = "cmy"
+        elif mes == "犯罪,戦争":
+            genre = ["crm","war"]
+        elif mes == "ドキュメンタリー":
+            genre = "doc"
+        elif mes == "ドラマ":
+            genre = "drm"
+        elif mes == "ファンタジー,SF":
+            genre = ["fnt","scf"]
+        elif mes == "歴史":
+            genre = "hst"
+        elif mes == "ホラー,ミステリー":
+            genre = ["hrr","trl"]
+        elif mes == "ファミリー":
+            genre = "fml"
+        elif mes == "ミュージカル":
+            genre = "msc"
+        elif mes == "ロマンス":
+            genre = "rma"
+        elif mes == "スポーツ":
+            genre = "spt"
+
+        else:
+            line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='選択肢の中から選んでください(右にスクロールできます)',
+                            quick_reply=QuickReply(items=[
+                                QuickReplyButton(action=MessageAction(label="アクション", text="アクション")),
+                                QuickReplyButton(action=MessageAction(label="アニメーション", text="アニメーション")),
+                                QuickReplyButton(action=MessageAction(label="コメディ", text="コメディ")),
+                                QuickReplyButton(action=MessageAction(label="犯罪,戦争", text="犯罪,戦争")),
+                                QuickReplyButton(action=MessageAction(label="ドキュメンタリー", text="ドキュメンタリー")),
+                                QuickReplyButton(action=MessageAction(label="ドラマ", text="ドラマ")),
+                                QuickReplyButton(action=MessageAction(label="ファンタジー,SF", text="ファンタジー,SF")),
+                                QuickReplyButton(action=MessageAction(label="歴史", text="歴史")),
+                                QuickReplyButton(action=MessageAction(label="ホラー,ミステリー", text="ホラー,ミステリー")),
+                                QuickReplyButton(action=MessageAction(label="ファミリー", text="ファミリー")),
+                                QuickReplyButton(action=MessageAction(label="ミュージカル", text="ミュージカル")),
+                                QuickReplyButton(action=MessageAction(label="ロマンス", text="ロマンス")),
+                                QuickReplyButton(action=MessageAction(label="スポーツ", text="スポーツ")),
+
+                            ])
+                            )
+            )
+            exit()
+
+
+        db.update({"genre": genre, "ques_id": 4}, query.id == user_id)
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text='評価を選んでください',
@@ -99,7 +215,7 @@ def handle_message(event: MessageEvent):
                             )
         )
 
-    elif db.search(query.id == user_id)[0]["ques_id"] == 3:
+    elif db.search(query.id == user_id)[0]["ques_id"] == 4:
         mes = event.message.text
         if mes == "星4以上":
             score = 8
@@ -107,9 +223,23 @@ def handle_message(event: MessageEvent):
             score = 7
         elif mes == "星3以上":
             score = 6
+        elif mes == "なんでもいい":
+            score = 0
         else:
-            sore = 0
-        db.update({"review_score": score, "ques_id": 4}, query.id == user_id)
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='選択肢の中から選んでください',
+                                quick_reply=QuickReply(items=[
+                                    QuickReplyButton(action=MessageAction(label="星4以上", text="星4以上")),
+                                    QuickReplyButton(action=MessageAction(label="星3.5以上", text="星3.5以上")),
+                                    QuickReplyButton(action=MessageAction(label="星3以上", text="星3以上")),
+                                    QuickReplyButton(action=MessageAction(label="なんでもいい", text="なんでもいい")),
+                                ])
+                                )
+            )
+            exit()
+
+        db.update({"review_score": score, "ques_id": 5}, query.id == user_id)
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text='検索を開始してよろしいですか？',
@@ -119,9 +249,18 @@ def handle_message(event: MessageEvent):
                             ])
                             )
         )
-    elif db.search(query.id == user_id)[0]["ques_id"] == 4:
-    # 以下にAPIを呼び出す処理を記載
-        exit()
+
+    elif db.search(query.id == user_id)[0]["ques_id"] == 5:
+        # 以下にAPIを呼び出す処理を記載
+        just_watch = JustWatch(country='JP')
+
+        # DBからとってきた値を格納
+        content_type = "movie"
+        provider = "nfx"
+        genre = "null"
+        score = 0
+
+        rec = Recommend(just_watch, content_type, provider, genre, score)
 
     else:
         line_bot_api.reply_message(
