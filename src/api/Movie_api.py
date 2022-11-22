dfbf058c7f5f2f68d4e021e3aa799c77b9fd5d3a
yoshimi-I <iwatsuki.443@outlook.com>
2022-11-23 02:03:40 +0900

    add: 年数による動画の絞り込みの実装

--- src/api/Movie_api.py
+++ src/api/Movie_api.py
@@ -6,12 +6,14 @@ import json
 
 
 class Recommend:
-    def __init__(self,just_watch,content_type,provider,genre,score):
+    def __init__(self,just_watch,content_type,provider,genre,score,start_year,end_year):
         self.just_watch = just_watch
         self.content_type = content_type
         self.provider = provider
         self.genre = genre
         self.num = score
+        self.start_year = start_year
+        self.end_year = end_year
         self.score = {
 						"imdb:score":
 							{
@@ -22,34 +24,68 @@ class Recommend:
         self.page_num = self.results["total_pages"]
         self.items = self.results["items"]
 
-    def top3(self):
+    def select_movies(self,num,page_num):
         L = []
-        results = self.just_watch.search_for_item(content_types=[self.content_type], providers=[self.provider],genres=[self.genre], scoring_filter_types=self.score, page=1,)
-        #
-        for i in range(3):
-            title = results["items"][i]["title"]
-            L.append(title)
-        return L
-    def top10(self):
-        L = []
-        results = self.just_watch.search_for_item(content_types=[self.content_type], providers=[self.provider],genres=[self.genre], scoring_filter_types=self.score, page=1)
-        items_num = len(results["items"])
+        movie_list = []
+        total_page = page_num
+        for i in range(total_page):
+            results = self.just_watch.search_for_item(content_types=[self.content_type], providers=[self.provider],genres=[self.genre], scoring_filter_types=self.score, page=i,release_year_from=self.start_year,release_year_until=self.end_year)
+            items_num = len(results["items"])
+            for j in range(items_num):
+                title = results["items"][j]["title"]
+                if title not in movie_list:
+                    movie_list.append(title)
+                if len(movie_list) >= num:
+                    break
+        return movie_list
 
-        # lenの中から選ぶべき3つの作品のインデックスをランダムで取得
-        num = random.sample(range(items_num), 3)
-        for i in range(items_num):
-            if i in num:
-                title = results["items"][i]["title"]
-                L.append(title)
+    def top_first(self):
+        L = []
+        choice_num = 3
+        movie_list = self.select_movies(choice_num,1)
+        if len(movie_list) <= choice_num:
+            choice_num = len(movie_list)
+        for k in range(choice_num):
+            L.append(movie_list[k])
         return L
+
+    def top_10(self):
+        L = []
+        choice_num = 10
+        movie_list = self.select_movies(choice_num,1)
+        output_num = 3
+        if len(movie_list) <= 3:
+            output_num = len(movie_list)
+        num = random.sample(range(len(movie_list)), output_num)
+        for k in num:
+            L.append(movie_list[k])
+        return L
+
+
+    def top_100(self):
+        L = []
+        choice_num = 100
+        movie_list = self.select_movies(choice_num,4)
+        output_num = 3
+        if len(movie_list) <= 3:
+            output_num = len(movie_list)
+        num = random.sample(range(len(movie_list)), output_num)
+        for k in num:
+            L.append(movie_list[k])
+        return L
+
+
 
     def recommend(self):
         L = []
+        choice_num = 3
         # ページの番号をランダムで取得して,そのページからとってくる(forのネスト回避)
         i = random.randint(0,self.page_num)
-        results2 = self.just_watch.search_for_item(content_types=[self.content_type],providers=[self.provider],genres=[self.genre],scoring_filter_types=self.score,page=i)
+        results2 = self.just_watch.search_for_item(content_types=[self.content_type],providers=[self.provider],genres=[self.genre],scoring_filter_types=self.score,page=i,release_year_from=self.start_year,release_year_until=self.end_year)
         items_num = len(results2["items"])
-        num = random.sample(range(items_num), 3)
+        if items_num <= choice_num:
+            choice_num = items_num
+        num = random.sample(range(items_num), choice_num)
         for j in range(items_num):
             if j in num:
                 title = results2["items"][j]["title"]
@@ -57,11 +93,14 @@ class Recommend:
         return L
 
     def info(self,choice_num):
+        url = ""
         watch_info = list()
         if choice_num == 0:
-            title_list = self.top3()
+            title_list = self.top_first()
         elif choice_num == 1:
-            title_list = self.top10()
+            title_list = self.top_10()
+        elif choice_num == 2:
+            title_list = self.top_100()
         else:
             title_list = self.recommend()
         title_num = len(title_list)
@@ -83,9 +122,10 @@ class Recommend:
 
             value_list  = title_info["items"][0]["scoring"]
             for j in range(len(value_list)):
-                value = value_list[j]["value"]
-                if self.num <= value <= 10:
-                    watch_info.append({"title":title_name,"url":url,"value":value,"content_type":self.content_type})
+                value_site = value_list[j]["provider_type"]
+                value_num = value_list[j]["value"]
+                if value_site == "imdb:score":
+                    watch_info.append({"title":title_name,"url":url,"value":value_num,"content_type":self.content_type})
                     break
 
         return watch_info
