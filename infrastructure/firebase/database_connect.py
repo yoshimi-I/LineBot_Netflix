533b068fe0d79544ce4d46de78fcfe1b5e068eda
yoshimi-I <iwatsuki.443@outlook.com>
2022-12-01 23:54:08 +0900

    clean: ディレクトリのリファクタリング(クリーンアーキテクチャの導入)

---
+++ infrastructure/firebase/database_connect.py
@@ -0,0 +1,12 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from typing import Dict, List, Tuple
+
+
+# firebaseの設定ファイルの読み込み
+class FirebaseConnect:
+    def __init__(self):
+        cred = credentials.Certificate("firebase-admin.json")
+        firebase_admin.initialize_app(cred)
+        db = firestore.client()
