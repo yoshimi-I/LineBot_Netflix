dede27a2373fc67ae5985b0e774271046e82c521
yoshimi-I <iwatsuki.443@outlook.com>
2022-12-02 18:47:51 +0900

    clean: コードの整形

--- usecase/services/main.py
+++ usecase/services/main.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 
 
 # interface　-> interfaces/services/main.pyで実装
-class MainFunc:
+class MainFunc(ABC):
+    @abstractmethod
     def handle_main_func(self,event: str, text: str, user_id: str, API_TOKEN: str, line_bot_api: str):
         pass
