c85f5827aae1ec5bc225115ad513d671b586c74d
yoshimi-I <iwatsuki.443@outlook.com>
2022-12-08 01:23:25 +0900

    delete: 不要な行の削除

--- domain/value_object/start_year.py
+++ domain/value_object/start_year.py
@@ -5,7 +5,6 @@ class StartYear:
     def __init__(self, start_year: int):
 
         if start_year < 0:
-            print("ques_id is not collect")
             raise ValueError("start_year is not collect")
         self.start_year = start_year
 
